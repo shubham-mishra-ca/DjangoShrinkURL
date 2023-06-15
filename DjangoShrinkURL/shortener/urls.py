f9267a9bf3a14b78e76103b62f80ff9506519af7
Shubham Mishra <smishra.on.ca@gmail.com>
2023-06-15 13:52:38 -0400

    Implemented user registration functionality

--- DjangoShrinkURL/shortener/urls.py
+++ DjangoShrinkURL/shortener/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import URLShortenerView 
+from .views import URLShortenerView, register
 
 urlpatterns = [
+    path('register/', register, name='register'),
     path('', URLShortenerView.as_view(), name='urlshortener'),
 ]
--- DjangoShrinkURL/shortener/views.py
+++ DjangoShrinkURL/shortener/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from django.views import View
 from .models import URL
 
@@ -8,5 +12,22 @@ class URLShortenerView(View):
 
     def post(self, request, *args, **kwargs):
         original_url = request.POST['original_url']
+        validate = URLValidator()
+        try:
+            validate(original_url)
+        except ValidationError:
+            return render(request, 'shortener/index.html', {'error': 'The provided URL is not valid.'})
+
         url, created = URL.objects.get_or_create(original_url=original_url)
         return render(request, 'shortener/index.html', {'url': url})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('urlshortener')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
